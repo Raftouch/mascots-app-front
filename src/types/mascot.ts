b798736a062a43b674cfc92e48be41574c90b024
raftouch <rafanelkin@gmail.com>
2026-03-06 15:30:45 +0100

    add mascot type

---
+++ src/types/mascot.ts
@@ -0,0 +1,12 @@
+import type { Collaborator } from "./collaborator";
+
+export type Mascot = {
+  _id: string;
+  name: string;
+  collaborator: Collaborator;
+  breed: string;
+  gender: string;
+  birthDate: string;
+  imageName?: string;
+  description: string;
+};
