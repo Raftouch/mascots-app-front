51a71aeeff86e2b83fec759fa41f1e767ea18864
raftouch <rafanelkin@gmail.com>
2026-03-06 16:42:24 +0100

    add route for collaborator details

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Collaborators from "./pages/Collaborators";
 import Mascots from "./pages/Mascots";
+import CollaboratorCard from "./components/CollaboratorCard";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/collaborators" element={<Collaborators />} />
+        <Route path="/collaborators/:id" element={<CollaboratorCard />} />
         <Route path="/mascots" element={<Mascots />} />
       </Routes>
     </div>
