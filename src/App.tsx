200280f3ae1acd88140b9307fe76e7f7bf95988b
raftouch <rafanelkin@gmail.com>
2026-03-06 15:16:59 +0100

    add route

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Collaborators from "./pages/Collaborators";
+import Mascots from "./pages/Mascots";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/collaborators" element={<Collaborators />} />
+        <Route path="/mascots" element={<Mascots />} />
       </Routes>
     </div>
   );
