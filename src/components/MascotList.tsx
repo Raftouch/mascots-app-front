0f19f80c596dc62bed6d96a8dc85b6783a173dce
raftouch <rafanelkin@gmail.com>
2026-03-06 15:43:43 +0100

    display mascots with full info (image to be added)

--- src/components/MascotList.tsx
+++ src/components/MascotList.tsx
@@ -1,18 +1,36 @@
-import { useEffect } from "react";
-
-const fetchMascots = async () => {
-  try {
-    const res = await fetch("http://localhost:4000/mascots");
-    const data = await res.json();
-    console.log("data mascots : ", data);
-  } catch (error) {
-    console.error("Error fetching mascots", error);
-  }
-};
+import { useEffect, useState } from "react";
+import type { Mascot } from "../types/mascot";
 
 export default function MascotList() {
+  const [mascots, setMascots] = useState<Mascot[]>([]);
+
+  const fetchMascots = async () => {
+    try {
+      const res = await fetch("http://localhost:4000/mascots");
+      const data = await res.json();
+      setMascots(data.mascots);
+      console.log("data mascots : ", data.mascots);
+    } catch (error) {
+      console.error("Error fetching mascots", error);
+    }
+  };
+
   useEffect(() => {
     fetchMascots();
   }, []);
-  return <div></div>;
+
+  return (
+    <ul>
+      {mascots.map((mascot) => (
+        <li key={mascot._id}>
+          <p>{mascot.name}</p>
+          <p>{mascot.breed}</p>
+          <p>{mascot.gender}</p>
+          <p>{mascot.birthDate}</p>
+          <p>{mascot.description}</p>
+          <p>{mascot.collaborator.name}</p>
+        </li>
+      ))}
+    </ul>
+  );
 }
