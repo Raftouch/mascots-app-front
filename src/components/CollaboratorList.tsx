94e23b5393b16b2394ca9f7b4abaacfba424e4ac
raftouch <rafanelkin@gmail.com>
2026-03-06 14:42:10 +0100

    display collaborators

--- src/components/CollaboratorList.tsx
+++ src/components/CollaboratorList.tsx
@@ -1,18 +1,28 @@
-import { useEffect } from "react";
-
-const fetchCollaborators = async () => {
-  try {
-    const res = await fetch("http://localhost:4000/collaborators");
-    const data = await res.json();
-    console.log("data : ", data);
-  } catch (error) {
-    console.error("Error fetching collaborators", error);
-  }
-};
+import { useEffect, useState } from "react";
+import type { Collaborator } from "../types/collaborator";
 
 export default function CollaboratorList() {
+  const [collaborators, setCollaborators] = useState<Collaborator[]>([]);
+
+  const fetchCollaborators = async () => {
+    try {
+      const res = await fetch("http://localhost:4000/collaborators");
+      const data = await res.json();
+      setCollaborators(data.collaborators);
+      console.log("data : ", data.collaborators);
+    } catch (error) {
+      console.error("Error fetching collaborators", error);
+    }
+  };
   useEffect(() => {
     fetchCollaborators();
   }, []);
-  return <div></div>;
+
+  return (
+    <ul>
+      {collaborators.map((collab) => (
+        <li key={collab._id}>{collab.name}</li>
+      ))}
+    </ul>
+  );
 }
