4e7c7f42364909d0160d6017698ed6681190ff9c
raftouch <rafanelkin@gmail.com>
2026-03-07 17:58:14 +0100

    fix dashboard route url, display last added mascots

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,22 +1,36 @@
-import { useEffect } from "react";
-
-const fetchMascots = async () => {
-  try {
-    const res = await fetch("http://localhost:4000/");
-    const data = await res.json();
-    console.log("data", data);
-  } catch (error) {
-    console.error("Error fetching mascots", error);
-  }
-};
+import { useEffect, useState } from "react";
+import type { Mascot } from "../types/mascot";
 
 export default function Dashboard() {
+  const [lastAddedMascots, setLastAddedMascots] = useState<Mascot[]>([]);
+
+  const getDashboard = async () => {
+    try {
+      const res = await fetch("http://localhost:4000/dashboard");
+      const data = await res.json();
+      console.log("data", data.mascots);
+      setLastAddedMascots(data.mascots);
+    } catch (error) {
+      console.error("Error fetching mascots", error);
+    }
+  };
+
   useEffect(() => {
-    fetchMascots();
+    getDashboard();
   }, []);
+
   return (
     <div>
       <h1>Dashboard</h1>
+      <div>
+        <h2>Recently added :</h2>
+
+        <ul>
+          {lastAddedMascots.map((mascot) => (
+            <li key={mascot._id}>{mascot.name}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
