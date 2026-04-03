dd3e9dad1df73097d248f3326f5d0d76e3291ba2
raftouch <rafanelkin@gmail.com>
2026-04-03 17:16:44 +0200

    update modal css

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -12,10 +12,10 @@ export default function Modal({ onDelete, onClose, onRedirect }: ModalProps) {
         onClick={onClose}
       />
       <div className="fixed min-w-[400px] min-h-[200px] p-10 rounded bg-white text-black top-40 left-1/2 -translate-x-1/2">
-        <h1 className="text-center mb-10">Are you sure ?</h1>
+        <h1 className="text-center mb-10">Are you sure?</h1>
         <div className="flex justify-center gap-20">
           <button
-            className="border rounded px-4 py-2 bg-green-500"
+            className="px-4 py-2 bg-red-700 text-white rounded-md hover:bg-red-800 font-medium"
             onClick={() => {
               onDelete();
               onClose();
@@ -25,7 +25,7 @@ export default function Modal({ onDelete, onClose, onRedirect }: ModalProps) {
             Yes
           </button>
           <button
-            className="border rounded px-4 py-2 bg-red-500"
+            className="px-4 py-2 bg-green-700 text-white rounded-md hover:bg-green-800 font-medium"
             onClick={onClose}
           >
             No
