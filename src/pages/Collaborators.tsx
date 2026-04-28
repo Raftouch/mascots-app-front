98d34225373ac02be423d66c875d50396ec35ec6
raftouch <rafanelkin@gmail.com>
2026-04-28 18:55:39 +0200

    feat(auth): fetch collaborators only when user is authenticated

--- src/pages/Collaborators.tsx
+++ src/pages/Collaborators.tsx
@@ -1,12 +1,17 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import CollaboratorList from "../components/CollaboratorList";
 import type { Collaborator } from "../types/collaborator";
 import { getCollaborators } from "../api/collaborators";
+import { AuthContext } from "../context/AuthContext";
+import { Navigate } from "react-router-dom";
 
 export default function Collaborators() {
   const [collaborators, setCollaborators] = useState<Collaborator[]>([]);
+  const { user, loading } = useContext(AuthContext);
 
   useEffect(() => {
+    if (loading || !user) return;
+
     const fetchCollaborators = async () => {
       try {
         const data = await getCollaborators();
@@ -18,8 +23,10 @@ export default function Collaborators() {
     };
 
     fetchCollaborators();
-  }, []);
+  }, [user, loading]);
 
+  if (loading) return <p>Loading...</p>;
+  if (!user) return <Navigate to="/login" />;
   if (collaborators.length === 0) return <p>No collaborators found</p>;
 
   return <CollaboratorList collaborators={collaborators} />;
