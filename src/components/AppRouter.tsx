6178fe6bc0527eff8705435b751523fd1de3109e
raftouch <rafanelkin@gmail.com>
2026-05-17 15:14:14 +0200

    refactor(router): extract routes into AppRouter component

---
+++ src/components/AppRouter.tsx
@@ -0,0 +1,33 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import Dashboard from "../pages/Dashboard";
+import Collaborators from "../pages/Collaborators";
+import Mascots from "../pages/Mascots";
+import CollaboratorCard from "../pages/CollaboratorDetails";
+import MascotCard from "../pages/MascotDetails";
+import Layout from "../components/Layout";
+import CreateMascot from "../pages/CreateMascot";
+import EditMascot from "../pages/EditMascot";
+import RegisterPage from "../pages/RegisterPage";
+import LoginPage from "../pages/LoginPage";
+
+export default function AppRouter() {
+  return (
+    <Routes>
+      <Route element={<Layout />}>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/collaborators" element={<Collaborators />} />
+        <Route path="/collaborators/:id" element={<CollaboratorCard />} />
+        <Route path="/mascots/:id" element={<MascotCard />} />
+        <Route path="/mascots/:id/edit" element={<EditMascot />} />
+        <Route path="/mascots" element={<Mascots />} />
+        <Route
+          path="/collaborators/:id/mascots/new"
+          element={<CreateMascot />}
+        />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Route>
+    </Routes>
+  );
+}
