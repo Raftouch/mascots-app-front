dc5353a85eb2bc8a1054c34c2835860a08bac368
raftouch <rafanelkin@gmail.com>
2026-05-17 16:22:22 +0200

    feat(router): restrict routes based on authentication

--- src/components/AppRouter.tsx
+++ src/components/AppRouter.tsx
@@ -1,15 +1,43 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import Layout from "../components/Layout";
-import { routes } from "../router";
+import { publicRoutes, privateRoutes } from "../router";
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
+import Loader from "./UI/Loader/Loader";
 
 export default function AppRouter() {
+  const { user, loading: authLoading } = useContext(AuthContext);
+
+  const isAuth = user ? true : false;
+
+  if (authLoading) return <Loader />;
+
   return (
     <Routes>
       <Route element={<Layout />}>
-        {routes.map((route) => (
-          <Route key={route.path} path={route.path} element={route.element} />
-        ))}
-        <Route path="*" element={<Navigate to="/" replace />} />
+        {isAuth ? (
+          <>
+            {privateRoutes.map((route) => (
+              <Route
+                key={route.path}
+                path={route.path}
+                element={route.element}
+              />
+            ))}
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </>
+        ) : (
+          <>
+            {publicRoutes.map((route) => (
+              <Route
+                key={route.path}
+                path={route.path}
+                element={route.element}
+              />
+            ))}
+            <Route path="*" element={<Navigate to="/login" replace />} />
+          </>
+        )}
       </Route>
     </Routes>
   );
